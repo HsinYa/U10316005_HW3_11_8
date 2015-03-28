919502b0731736a5ddcacfdbb3fca87fda9f0f83
HsinYa <starrysky012499@gmail.com>
2015-03-28 17:16:48 +0800

    Design a class named Account
    The class Account contain:
    four variables; two constructors;
    three methods for setting id, balance, annual interest rate;
    four methods for getting id, balance, annual interest rate,date;
    one method for  getting monthly interest rate;
    one method for getting monthly interest;
    one method for withdraw amounts from the account;
    one method for deposit amounts to the account.

---
+++ Account.java
@@ -0,0 +1,62 @@
+
+public class Account {
+	//Data fields
+	private int id = 0;
+	private double balance = 0;
+	private double annualInterestRate = 0;
+	private java.util.Date dateCreated;
+	
+	//Constructors
+	public Account(){
+		dateCreated = new java.util.Date();
+	}
+	public Account(int id,double balance){
+		this.id = id;
+		this.balance = balance;
+		dateCreated = new java.util.Date();
+	}
+	
+	//Accessor and mutator methods for id, balance, annualInterestRate
+	public void setId(int id){
+		this.id = id;
+	}
+	public void setBalance(double balance){
+		this.balance = balance;
+	}
+	public void setAnnualInterestRate(double annualInterestRate){
+		this.annualInterestRate = annualInterestRate;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	public double getBalance(){
+		return balance;
+	}
+	public double getAnnualInterestRate(){
+		return annualInterestRate;
+	}
+	
+	//The accessor method for dateCreated
+	public java.util.Date getDateCreated(){
+		return dateCreated ;
+	}
+	//Return the monthly interest rate
+	public double getMonthlyInterestRate(){
+		return annualInterestRate/1200 ;
+	}
+	//Return the monthly interest
+	public double getMonthlyInterest(){
+		return balance*(annualInterestRate/1200);
+	}
+	
+	//Withdraw a specified amount from the account
+	public void withdraw(double amount){
+		balance -= amount;
+	}
+	//Deposit a specified amount to the account
+	public void deposit(double amount){
+		balance += amount;
+	}
+	
+}
